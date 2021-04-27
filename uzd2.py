f74973da45e066926fb42175858288e021bbcf93
Valdis-tech <78405534+Valdis-tech@users.noreply.github.com>
2021-04-27 09:36:09 +0300

    Update uzd2.py

--- uzd2.py
+++ uzd2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*- 
 """ 
 Uzrakstiet programmu, kas ielasa vienu vārdu 
 un izvada uz ekrāna sveicienu sekojošā formātā:
@@ -5,3 +6,12 @@ un izvada uz ekrāna sveicienu sekojošā formātā:
 Ja ievadīts nav jūsu vards, tiek izdrukāts teksts - Uzredzēšanos!
 Pārbaudiet programmas darbību ar dažādiem ievaddatiem.
 """
+def ps()
+
+vards=float(input("Tavs vārds:"))
+
+if vards=Valdis print("Labdien, Valdi, pirmdienā!")
+
+else: print ("Uzredzēšanos")
+
+ps()
