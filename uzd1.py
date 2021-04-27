7521569fe3cae4b361d23d5d0af97ecd6dc3e361
Valdis-tech <78405534+Valdis-tech@users.noreply.github.com>
2021-04-27 09:29:06 +0300

    Update uzd1.py

--- uzd1.py
+++ uzd1.py
@@ -1,8 +1,18 @@
-
+# -*- coding: utf-8 -*- 
 """ 
 Uzrakstiet programmu, kas ielasa skaitli (kā float) -
 riņķa līnijas rādiusu un izvada uz ekrāna (print) 
 riņķa līnijas garumu un laukumu, atbilstoši noformējot atbildi.
 Pārbaudiet programmas darbību ar dažādiem ievaddatiem.
 """
+from math import pi
 
+radiuss=float(input("Ievadi rādiusu:"))
+
+garums=2*pi*radiuss
+
+laukums=pi*radiuss*2
+
+print(f'riņķa līnijas garums ir {garums}')
+
+print(f'riņka līnijas laukums ir {laukums}')
