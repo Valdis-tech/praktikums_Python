c3ec03ccb3d1cb8308721dbc5e554d6baa635ba4
Valdis-tech <78405534+Valdis-tech@users.noreply.github.com>
2021-04-27 09:18:52 +0300

    Update uzd1.py

--- uzd1.py
+++ uzd1.py
@@ -1,16 +1,8 @@
-# -*- coding: utf-8 -*- 
+
 """ 
 Uzrakstiet programmu, kas ielasa skaitli (kā float) -
 riņķa līnijas rādiusu un izvada uz ekrāna (print) 
 riņķa līnijas garumu un laukumu, atbilstoši noformējot atbildi.
 Pārbaudiet programmas darbību ar dažādiem ievaddatiem.
 """
-n=float(input("Ievadi skaitli"))
 
-nn=n+n
-
-nnn=n+n+n
-
-summa=n+nn+nnn
-
-print(summa)
